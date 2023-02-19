749654976a06e2d1a8a24e28078c63a2004f73c1
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-02-19 06:37:49 -0300

    feat: ✨ first method in core program - find the contact in agenda

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
+AGENDA = {}
 
+AGENDA['frajola'] = {
+    'tel': '1234',
+    'mail': 'frajola@gmail.com',
+    'address': 'Av. 1',
+}
+
+def list_contacts():
+    print("CONTACTS: ")
+    print("#####################################")
+    for contact in AGENDA:
+        print(f"Name: {contact}")
+        print(f"Phone: {AGENDA[contact]['tel']}")
+        print(f"E-mail: {AGENDA[contact]['mail']}")
+        print(f"Address: {AGENDA[contact]['address']}")
+        print("#####################################")
+
+def find_contact():
+    name = input("Write the contact name: ")
+    try:
+        contact = AGENDA[name]
+        print("#####################################")
+        print(f'FIND THE CONTACT: {name}')
+        print("#####################################")
+        print(f"Name: {name}")
+        print(f"Phone: {contact['tel']}")
+        print(f"E-mail: {contact['mail']}")
+        print(f"Address: {contact['address']}")
+    except:
+        print("Contact not found")
 
 if __name__ == "__main__":
-    print('CONTACT AGENDA...')
+    print('******* CONTACT AGENDA ******\n')
+    find_contact()
