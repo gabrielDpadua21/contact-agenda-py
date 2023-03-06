9a473bc6d98add7bfc4976e88128ec29df750108
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-03-06 04:05:34 -0300

    feat: ✨ create functions to read e write in files

--- open_files.py
+++ open_files.py
@@ -1,5 +1,21 @@
-try:
-    with open('contacts.txt', 'r') as file:
-        print(file.readlines())
-except FileNotFoundError:
-    print('File not found')
+
+def read_files():
+    try:
+        with open('contacts.txt', 'r') as file:
+            print(file.readlines())
+    except FileNotFoundError:
+        print('File not found')
+    except Exception as error:
+        print('Unexpeted error')
+        print(error)
+
+
+def append_files(name):
+    try:
+        with open('contacts.txt', 'a') as file:
+            file.write(f'{name}\n')
+    except FileNotFoundError:
+        print('file not found')
+    except Exception as error:
+        print('Unexpeted error')
+        print(error)
